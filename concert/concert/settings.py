6814b84836cec684e4b84193fffde89a1fbbd907
mohammadjawid143 <m.jawid110@gmail.com>
2026-05-29 20:42:20 +0330

    fix allowed hosts

--- concert/concert/settings.py
+++ concert/concert/settings.py
@@ -27,11 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [
-    'concert-8njb.onrender.com',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
